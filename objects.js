e97eacfc3a6d5e27e37f9e0af566ec6c421caf45
Michele Rawlins <shellbop@att.net>
2020-01-20 19:53:41 -0600

    added object

--- objects.js
+++ objects.js
@@ -1,1 +1,21 @@
 console.log("whoops");
+
+const wordLength = (word) => {
+return word.length;
+};
+
+console.log (wordLength('cat'));
+console.log(wordLength('monkeybutt'));
+
+const ageCheck = (age) => {
+if (age >= 21) {
+ console.log('Party!!!!!!');
+
+} else { 
+    console.log('No drinks for you');
+
+}
+};
+
+ageCheck(12)
+ageCheck(24)

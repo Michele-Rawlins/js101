16280f8f5cefdb214a86f0d1d64dc0637b04a52c
Michele Rawlins <shellbop@att.net>
2020-01-20 20:44:10 -0600

    added more objects js

--- objects.js
+++ objects.js
@@ -17,5 +17,40 @@ if (age >= 21) {
 }
 };
 
-ageCheck(12)
-ageCheck(24)
+ageCheck(12);
+ageCheck(24);
+
+const expense = {
+dateCreated: '01/17/2020',
+location: 'Baja Burrito',
+cost: 12.57,
+isGood: true,
+};
+
+//dot notation
+console.log(expense.cost);
+
+//bracket notation
+console.log(expense['cost']);
+
+const employee = {
+    id: '1234mar',
+    dateHired:  '1/05/2020',
+    passDrugScreen: true,
+    department: 'shipping',
+    city: 'clarksville',
+    firstName: 'Bob',
+    lastName: 'Hope',
+    salary:  75000,
+    supervisor: '456dar', 
+};
+const vip =(emp ) => {
+if (emp.firstName[0] === 'M') {
+
+emp.status = 'vip';
+} else {
+    emp.status = 'Peasant';
+}
+return emp;
+};
+console.log(vip(employee));

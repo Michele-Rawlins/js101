8aeb0b1706bf2cedc9da5f02faacf2a9fb0a294f
Michele Rawlins <shellbop@att.net>
2020-01-21 18:16:43 -0600

    added challenges

--- combo.js
+++ combo.js
@@ -1,1 +1,41 @@
 console.log("combos");
+
+const evenOdd = (num) => {
+if (num % 2 == 0) {  
+return 'Even'
+} else {
+    return 'Odd'
+}
+}; 
+
+console.log(evenOdd(5));
+
+const opposite = (number) => {
+    return(-number);
+};
+
+console.log(opposite(4));
+
+const palindrome = (text) => {
+    const reversedText  = text.toLowerCase()
+     .split('').reverse().join('');
+if (text === reversedText) {
+return true
+} else {
+    return false
+}
+};
+
+    
+console.log(palindrome('plane'));
+
+const lawrence = (animal) => {
+
+    if (animal === 'alligator') {
+        return 'small'
+    }else{
+        return 'wide'
+    }
+
+};
+console.log(lawrence("rabbit"));
